550cbfa4bb7a2cccc3e0e27b2e8497fe47aa5a7b
dosfx <andrew.adbrown@gmail.com>
2020-05-31 13:25:29 -0400

    going vertical

--- mahjong.js
+++ mahjong.js
@@ -11,6 +11,41 @@ const TILE_PADDING = 4;
 const OFFSET = 12345567890;
 
 Vue.prototype.$firstTile = null;
+Vue.prototype.$layouts = {
+    classic: function() {
+        let layout = Array.prototype.concat(
+            // bottom layer, row by row
+            Array.from({length:12}, function (_v, i) { return [i + 1, 0, 0]; }),
+            Array.from({length:8}, function (_v, i) { return [i + 3, 1, 0]; }),
+            Array.from({length:10}, function (_v, i) { return [i + 2, 2, 0]; }),
+            [[0, 3.5, 0]],
+            Array.from({length:12}, function (_v, i) { return [i + 1, 3, 0]; }),
+            Array.from({length:12}, function (_v, i) { return [i + 1, 4, 0]; }),
+            [[13, 3.5, 0], [14, 3.5, 0]],
+            Array.from({length:10}, function (_v, i) { return [i + 2, 5, 0]; }),
+            Array.from({length:8}, function (_v, i) { return [i + 3, 6, 0]; }),
+            Array.from({length:12}, function (_v, i) { return [i + 1, 7, 0]; }),
+            // second layer square
+            Array.from({length:36}, function (_v, i) { return [(i % 6) + 4, Math.floor(i / 6) + 1, 1]; }),
+            // third layer square
+            Array.from({length:16}, function (_v, i) { return [(i % 4) + 5, Math.floor(i / 4) + 2, 2]; }),
+            // forth layer square
+            Array.from({length:4}, function (_v, i) { return [(i % 2) + 6, Math.floor(i / 2) + 3, 3]; }),
+            [[6.5, 3.5, 4]]
+        );
+
+        let count = 144 - layout.length;
+        for (let i = 0; i < count; i++) {
+            layout.push([0,10,i]);
+        }
+
+        return layout;
+    },
+    flat: function() {
+        return Array.from({length:144}, function (_v, i) { return [i % 16, Math.floor(i / 16), 0]; });
+    }
+};
+
 var mahjong = new Vue({
     el: "#mahjong",
     data: {
@@ -36,16 +71,20 @@ var mahjong = new Vue({
                 }
             }
 
+            // get the layout
+            let layout = this.$layouts.classic();
             // fill the board
-            shuffle.forEach((t, i) => {
+            shuffle.forEach(function (t, i) {
+                t.x = layout[i][0];
+                t.y = layout[i][1];
+                t.z = layout[i][2];
+
                 t.selected = false;
-                t.x = i % 16;
-                t.y = Math.floor(i / 16);
-                t.z = 0;
                 t.style = {
                     backgroundPosition: (t.num * -TILE_WIDTH) + "px " + (t.suit * -TILE_HEIGHT) + "px",
-                    left: ((t.x * (TILE_WIDTH + TILE_PADDING)) + TILE_PADDING) + "px",
-                    top: ((t.y * (TILE_HEIGHT + TILE_PADDING)) + TILE_PADDING) + "px"
+                    left: ((t.x * (TILE_WIDTH + TILE_PADDING)) + TILE_PADDING - (t.z * 3)) + "px",
+                    top: ((t.y * (TILE_HEIGHT + TILE_PADDING)) + TILE_PADDING - (t.z * 6)) + "px",
+                    zIndex: t.z
                 }
             });
 
